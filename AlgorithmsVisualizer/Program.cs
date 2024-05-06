137c63fd677c07c85072e1c5ba0d76212c2d4014
Brent Reynolds <brentsreynolds5@gmail.com>
2024-05-05 21:21:25 -0400

    Added UI for choosing which algorithm to use

--- AlgorithmsVisualizer/Program.cs
+++ AlgorithmsVisualizer/Program.cs
@@ -14,19 +14,79 @@
             Console.WriteLine("7. Radix Sort");
             Console.WriteLine("8. Counting Sort");
             Console.WriteLine("9. Bucket Sort");
+            Console.WriteLine("To quit, type 'exit'.");
             string algorithmSelection = Console.ReadLine();
-            Console.WriteLine("Please enter a list of numbers that you would like to have sorted: ");
-            string numberList = Console.ReadLine();
-
-            List<int> numbers = new List<int>();
-            foreach (string number in numberList.Split(' '))
+            while (algorithmSelection != "exit")
             {
-                numbers.Add(int.Parse(number));
-            }
+                Console.WriteLine("Please enter a list of numbers that you would like to have sorted: ");
+                string numberList = Console.ReadLine();
 
-            foreach (int number in numbers)
-            {
-                Console.Write(number + " ");
+                List<int> numbers = new List<int>();
+                foreach (string number in numberList.Split(' '))
+                {
+                    numbers.Add(int.Parse(number));
+                }
+
+                switch (algorithmSelection)
+                {
+                    case "1":
+                        Algorithms.BubbleSort(numbers);
+                        numberList = Algorithms.ListToString(numbers);
+                        Console.WriteLine("Sorted list: " + numberList);
+                        break;
+                    case "2":
+                        Algorithms.SelectionSort(numbers);
+                        numberList = Algorithms.ListToString(numbers);
+                        Console.WriteLine("Sorted list: " + numberList);
+                        break;
+                    case "3":
+                        Algorithms.InsertionSort(numbers);
+                        numberList = Algorithms.ListToString(numbers);
+                        Console.WriteLine("Sorted list: " + numberList);
+                        break;
+                    case "4":
+                        Algorithms.MergeSort(numbers);
+                        numberList = Algorithms.ListToString(numbers);
+                        Console.WriteLine("Sorted list: " + numberList);
+                        break;
+                    case "5":
+                        Algorithms.QuickSort(numbers);
+                        numberList = Algorithms.ListToString(numbers);
+                        Console.WriteLine("Sorted list: " + numberList);
+                        break;
+                    case "6":
+                        Algorithms.HeapSort(numbers);
+                        numberList = Algorithms.ListToString(numbers);
+                        Console.WriteLine("Sorted list: " + numberList);
+                        break;
+                    case "7":
+                        Algorithms.RadixSort(numbers);
+                        numberList = Algorithms.ListToString(numbers);
+                        Console.WriteLine("Sorted list: " + numberList);
+                        break;
+                    case "8":
+                        Algorithms.CountingSort(numbers);
+                        numberList = Algorithms.ListToString(numbers);
+                        Console.WriteLine("Sorted list: " + numberList);
+                        break;
+                    case "9":
+                        Algorithms.BucketSort(numbers);
+                        numberList = Algorithms.ListToString(numbers);
+                        Console.WriteLine("Sorted list: " + numberList);
+                        break;
+                }
+
+                Console.WriteLine("1. Bubble Sort");
+                Console.WriteLine("2. Selection Sort");
+                Console.WriteLine("3. Insertion Sort");
+                Console.WriteLine("4. Merge Sort");
+                Console.WriteLine("5. Quick Sort");
+                Console.WriteLine("6. Heap Sort");
+                Console.WriteLine("7. Radix Sort");
+                Console.WriteLine("8. Counting Sort");
+                Console.WriteLine("9. Bucket Sort");
+                Console.WriteLine("To quit, type 'exit'.");
+                algorithmSelection = Console.ReadLine();
             }
 
         }
