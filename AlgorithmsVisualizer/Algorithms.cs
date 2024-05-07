e945ddaed2ccfe531f9f7b5b3afd973e8e4bc25b
Brent Reynolds <brentsreynolds5@gmail.com>
2024-05-06 21:41:47 -0400

    Added methods to do merge sort

--- AlgorithmsVisualizer/Algorithms.cs
+++ AlgorithmsVisualizer/Algorithms.cs
@@ -68,9 +68,45 @@ namespace AlgorithmsVisualizer
                 }   
             }   
         }
-        public static void MergeSort(List<int> numbers)
+        public static void MergeSort(int[] arr)
         {
+            if (arr.Length <= 1)
+                return;
 
+            int mid = arr.Length / 2;
+
+            int[] left = new int[mid];
+            int[] right = new int[arr.Length - mid];
+
+            for (int i = 0; i < mid; i++)
+                left[i] = arr[i];
+
+            for (int i = mid; i < arr.Length; i++)
+                right[i - mid] = arr[i];
+
+            MergeSort(left);
+            MergeSort(right);
+
+            Merge(arr, left, right);
+        }
+
+        private static void Merge(int[] arr, int[] left, int[] right)
+        {
+            int i = 0, j = 0, k = 0;
+
+            while (i < left.Length && j < right.Length)
+            {
+                if (left[i] <= right[j])
+                    arr[k++] = left[i++];
+                else
+                    arr[k++] = right[j++];
+            }
+
+            while (i < left.Length)
+                arr[k++] = left[i++];
+
+            while (j < right.Length)
+                arr[k++] = right[j++];
         }
         public static void QuickSort(List<int> numbers)
         {
@@ -96,6 +132,15 @@ namespace AlgorithmsVisualizer
         {
             StringBuilder sb = new StringBuilder();
             foreach (int number in numbers)
+            {
+                sb.Append(number + " ");
+            }
+            return sb.ToString();
+        }
+        public static string ArrayToString(int[] array)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (int number in array)
             {
                 sb.Append(number + " ");
             }
